fade68f223dd20586d3e0329771ca477b1a014fa
FilipKubackiSoton <56367856+FilipKubackiSoton@users.noreply.github.com>
2020-06-17 20:01:07 +0200

    Add files via upload
    Two plots improvement: trajectory and solution development

--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 import numpy as np 
 import math 
-import copy 
-import tensorflow as tf
+import copy
+import matplotlib.pyplot as plt 
+import matplotlib.cm as cm
 
 def rand_bin_array(K, N):
     """
@@ -14,11 +15,13 @@ def rand_bin_array(K, N):
     return arr
 
 
+
 def hiff_fitness(array):
     """
     Calculate and return value related to h-iff 
     assignment to the binary string of array. 
     """
+
 
     def f(val):
         if val == 1 or val == 0:
@@ -50,6 +53,7 @@ def hiff_fitness(array):
     levels = int(math.log2(size))
     sum = 0
     return val_recursive(array, 0,  sum)
+        
 
 def generate_training_sat(N, set_size):
     """
@@ -57,14 +61,15 @@ def generate_training_sat(N, set_size):
     
     return: binary array of size N to train NN
     """
-    input = np.ndarray(shape=(set_size, N))
-    output = np.ndarray(shape=(set_size, N))
+    input = []
+    output = []
+    sol_evol = []
 
     if not (math.log2(N)).is_integer():
             raise ValueError("Array size must be power of 2.")
     for k in range(set_size):
         candidate_solution = np.random.randint(2, size = N)
-        input[k]=candidate_solution
+        input.append(candidate_solution)
         solution_fitness = hiff_fitness(candidate_solution)
         for i in range(10 * N):
             index = np.random.randint(N)
@@ -74,6 +79,57 @@ def generate_training_sat(N, set_size):
             if new_fitness >= solution_fitness : 
                 candidate_solution = new_candidate_sol
                 solution_fitness = new_fitness
-        output[k]=candidate_solution
-
     return input, output
+
+def generate_evol_plot(N=32, path = "solution_development_plot.png", learning_steps = 50):
+    candidate_solution = np.random.randint(2, size = N)
+    sol_evol = []
+    max_fitt = 0
+    for i in range(learning_steps):
+        index = np.random.randint(N)
+        current_fit = hiff_fitness(candidate_solution)
+        index = np.random.randint(N)
+        new_candidate_sol = copy.copy(candidate_solution)
+        new_candidate_sol[index] = 1 - new_candidate_sol[index]
+        new_fit = hiff_fitness(candidate_solution)
+        if new_fit >= current_fit:
+            sol_evol.append(new_candidate_sol)
+            candidate_solution = new_candidate_sol
+
+    tmp = np.asarray(sol_evol)
+    plt.figure()
+    plt.imshow(tmp, interpolation='nearest', cmap=cm.Greys_r)
+    plt.title("Solution Development at Evolution Step 1")
+    plt.xlabel("Solution variable")
+    plt.ylabel("Development Step")
+    plt.colorbar()
+    plt.savefig(path)
+
+def generate_sol_plot(N=32, target_size = 10 ,path = "trajectory_plot.png", learning_steps = 70):    
+    X = np.arange(learning_steps)
+    normalization_factor = hiff_fitness(np.ones((N,)))
+    plt.figure()
+    plt.title("Example Solution Trajectory at Evolution Step 1")
+    for k in range(target_size):
+        candidate_solution = np.random.randint(2, size = N)
+        solution_fitness = hiff_fitness(candidate_solution)
+        current_target_trajectory = [solution_fitness/normalization_factor]
+        for i in range(learning_steps-1):
+            index = np.random.randint(N)
+            new_candidate_sol = copy.copy(candidate_solution)
+            new_candidate_sol[index] = 1 - new_candidate_sol[index] # apply variation 
+            new_fitness = hiff_fitness(new_candidate_sol) # check the change 
+            if new_fitness >= solution_fitness : 
+                candidate_solution = new_candidate_sol
+                solution_fitness = new_fitness
+                current_target_trajectory.append(new_fitness/normalization_factor)
+            else:
+                current_target_trajectory.append(current_target_trajectory[-1])
+        plt.plot(X, current_target_trajectory)
+    plt.xlabel("learning step")
+    plt.ylabel("fitness \ max_fitness")
+    plt.savefig(path)
+
+generate_sol_plot()
+generate_evol_plot()
+    
